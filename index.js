5a275f7b08f4867995c5b56f2f88d936fb983e51
hhotma <mat.hodek@gmail.com>
2024-10-07 23:55:25 +0200

    cleanup

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ import mongoose from "mongoose";
 
 import authRoute from "./routes/auth.js";
 import userRoute from "./routes/user.js";
-import generateRoute from "./routes/gen.js";
+import genRoute from "./routes/gen.js";
 
 dotenv.config()
 
 const api = express()
 
+// database init
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
@@ -26,13 +27,17 @@ mongoose.connection.on("connected", () => {
     console.log("mongoDB connected..");
 })
 
-api.use(express.json());
 
+// setup middlewares
+api.use(express.json());
+api.use(cors());
+
+// route middlewares
 api.use("/auth", authRoute);
 api.use("/user", userRoute);
-api.use("/gen", generateRoute);
+api.use("/gen", genRoute);
 
-
+// initiation
 api.listen(8800, () => {
     connectDB();
     console.log("backend initiated..")
